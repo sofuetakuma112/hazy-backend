540d546aaaab7b479d806fabaa20b4c3531e88f1
sofuetakuma <kawahagi0620@gmail.com>
2022-05-03 15:20:29 +0900

    認証用のミドルウェア関数とログイン機能実装

--- server/main.go
+++ server/main.go
@@ -24,27 +24,65 @@ func handleTest(w http.ResponseWriter, r *http.Request) {
 	message := Message{
 		Message: "this is test",
 	}
-	output, err := json.MarshalIndent(&message, "", "\t\t")
+	json_bytes, err := json.MarshalIndent(&message, "", "\t\t")
 	if err != nil {
 		panic(err)
 	}
 	w.Header().Set("Content-type", "application/json")
-	w.Write(output)
+	w.Write(json_bytes)
 }
 
-func handleGetUser(w http.ResponseWriter, r *http.Request) {
-	user := User{}
-	db.First(&user, 1)
-	output, err := json.MarshalIndent(&user, "", "\t\t")
+func handleGetCurrentUser(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("jwt")
 	if err != nil {
-		panic(err)
+		http.Error(w, fmt.Sprintf("%v", err), 400)
+		return
 	}
+	jwt := cookie.Value
+
+	claims, err := verificateJWT(jwt) // JWTの署名を検証してOKならclaimsを返す
+	if err != nil {
+		http.Error(w, fmt.Sprintf("%v", err), 400)
+		return
+	}
+
+	var user User
+	db.First(&user, claims["user_id"])
 	w.Header().Set("Content-type", "application/json")
-	w.Write(output)
+	json_bytes, _ := json.MarshalIndent(&user, "", "\t\t")
+	w.Write(json_bytes)
 }
 
 func handleLogin(w http.ResponseWriter, r *http.Request) {
-	//
+	if r.Method == http.MethodPost {
+		len := r.ContentLength
+		body := make([]byte, len)
+		r.Body.Read(body)
+		// TODO: JSONデータのキーが正しいかチェックする必要がある
+		m := make(map[string]interface{})
+		err := json.Unmarshal(body, &m)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("%v", err), 400)
+			return
+		}
+		fmt.Println(m)
+
+		var user User
+		if err := db.Where("email = ? AND password = ?", m["email"], m["password"]).First(&user).Error; err != nil {
+			http.Error(w, fmt.Sprintf("%v", err), 400)
+			return
+		}
+		// user.IDを元にJWTを生成する
+		jwt := generateJWT(int(user.ID))
+		cookie := &http.Cookie{
+			Name:  "jwt", // ここにcookieの名前を記述
+			Value: jwt,   // ここにcookieの値を記述
+		}
+		http.SetCookie(w, cookie)
+		w.Header().Set("Content-type", "application/json")
+		json_bytes, _ := json.MarshalIndent(&user, "", "\t\t")
+		w.Write(json_bytes)
+	}
 }
 
 func handleCreateUser(w http.ResponseWriter, r *http.Request) {
@@ -68,13 +106,33 @@ func handleCreateUser(w http.ResponseWriter, r *http.Request) {
 		}
 		http.SetCookie(w, cookie)
 		w.Header().Set("Content-type", "application/json")
-		output, err := json.MarshalIndent(&user, "", "\t\t")
+		json_bytes, err := json.MarshalIndent(&user, "", "\t\t")
 		if err != nil {
 			panic(err)
 		}
-		w.Write(output)
+		w.Write(json_bytes)
 	} else {
 		// 対応していないHttpメソッドなのでエラーを返す
+	}
+}
+
+func authMiddleWare(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// JWTが有効かチェックする
+		cookie, err := r.Cookie("jwt")
+		if err != nil {
+			// クッキーが無い時のエラー？
+			http.Error(w, fmt.Sprintf("%v", err), 400)
+			return
+		}
+		jwt := cookie.Value
+
+		_, err = verificateJWT(jwt) // JWTの署名を検証してOKならclaimsを返す
+		if err != nil {
+			http.Error(w, fmt.Sprintf("%v", err), 401)
+			return
+		}
+		f(w, r)
 	}
 }
 
@@ -97,15 +155,8 @@ func main() {
 		Addr: ":8080",
 	}
 	http.HandleFunc("/test", handleTest)
-	http.HandleFunc("/getUser", handleGetUser)
+	http.HandleFunc("/getCurrentUser", handleGetCurrentUser)
 	http.HandleFunc("/createUser", handleCreateUser)
+	http.HandleFunc("/login", handleLogin)
 	server.ListenAndServe()
-
-	// jwt := generateJWT(1)
-	// claims, err := verificateJWT(jwt)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	// http.Error(w, fmt.Sprintf("...: %w", err) , 400)
-	// }
-	// fmt.Println(claims)
 }
